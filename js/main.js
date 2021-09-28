5e2acee8ce25e9396e4fc27c471f7eaf86cc01c0
Hamed Asadi Asl <90451086+hamed3958@users.noreply.github.com>
2021-09-28 18:07:39 +0330

    complete the responsive mode

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     for (let j = 0; j < acc.length; j++) {
-      if(j!=i){
+      if (j != i) {
         acc[j].classList.remove("active");
         acc[j].nextElementSibling.style.maxHeight = null;
       }
     }
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
+    if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
