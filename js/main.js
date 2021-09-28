470ea59d10e66ba88c6a2d1aabecad5e5388e6df
Hamed Asadi Asl <90451086+hamed3958@users.noreply.github.com>
2021-09-28 15:30:24 +0330

    debug the js, desktop mode

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 var acc = document.getElementsByClassName("accordion");
 var i;
 
-for (i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
+    for (let j = 0; j < acc.length; j++) {
+      if(j!=i){
+        acc[j].classList.remove("active");
+        acc[j].nextElementSibling.style.maxHeight = null;
+      }
+    }
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
+    if (panel.style.maxHeight){
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
